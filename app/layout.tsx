86543083ddf6c8d36ab0833fe6d594f88280494b
Nishant <alpha.5188457@gmail.com>
2023-12-09 12:09:22 +0530

    Navbar Created with authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import { Nunito } from 'next/font/google'
 import './globals.css'
+import ClientOnly from './components/ClientOnly'
+import LoginModal from './components/modals/LoginModal'
+import RegisterModal from './components/modals/RegisterModal'
+import Navbar from './components/navbar/Navbar'
+import ToasterProvider from './providers/ToasterProvider'
+import getCurrentUser from './actions/getCurrentUser'
 
 export const metadata = {
   title: 'Airbnb Clone',
@@ -10,14 +16,26 @@ const font = Nunito({
   subsets: ['latin'] 
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
